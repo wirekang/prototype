4d996a468f199a0d434e784a722df14ea4c18e2c
wirekang <wirekang@gmail.com>
2022-06-27 20:30:42 +0900

    Test Commit 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4444, '0.0.0.0');
-  // TEST
+  // TEST 2
 }
 bootstrap();
