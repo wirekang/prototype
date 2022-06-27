dc8c687ad67175feb7db6562b068fc8727ef1e57
wirekang <wirekang@gmail.com>
2022-06-27 20:43:11 +0900

    TestCommit 4

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '333333 ' + process.env.NODE_ENV + '....';
+    return 'Hello World!';
   }
 }
