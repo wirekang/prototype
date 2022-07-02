f79fa34de0ba9d10cab7ec1ff55f22b9fd8affd9
wirekang <wirekang@gmail.com>
2022-07-02 15:14:33 +0900

    a

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `NODE_ENV: ${process.env.NODE_ENV}<br/>config: <br/>${fs
+    return `\nNODE_ENV: ${process.env.NODE_ENV}<br/>config: <br/>${fs
       .readFileSync('config.json')
       .toString()}`;
   }
